88c96b2079412a6ca78da66ffea5bb549db9fd3e
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2023-10-09 14:49:04 -0300

    Configuração inicial do projeto

---
+++ mediSearchAdmin/settings/production.py
@@ -0,0 +1,14 @@
+from .settings import *
+DEBUG = False
+
+SECRET_KEY = 'E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855'
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+    }
+}
+
