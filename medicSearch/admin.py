671afecf688d67ca15a985f959cf85fe3e53502b
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2023-10-10 09:24:05 -0300

    feat: Customizando o painel admin para profiles

--- medicSearch/admin.py
+++ medicSearch/admin.py
@@ -2,7 +2,46 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Profile)
+
+class ProfileAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    list_display = ('user', 'role', 'birth',
+                    'specialitiesList', 'addressesList')
+    empty_value_display = 'Vazio'
+    list_filter = ('user__is_active',)
+    search_fields = ('user__username',)
+
+    fieldsets = (
+        ('Usuario', {
+            'fields': ('user', 'birthday', 'image')
+        }),
+        ('Função', {
+            'fields': ('role',)
+        }),
+        ('Extras', {
+            'fields': ('specialites', 'addresses')
+        })
+    )
+
+    def birth(self, obj):
+        if (obj.birthday):
+            return obj.birthday.strftime("%d/%m/%Y")
+    birth.empty_value_display = '___/___/_____'
+
+    def specialitiesList(self, obj):
+        return [i.name for i in obj.specialites.all()]
+
+    def addressesList(self, obj):
+        return [i.name for i in obj.addresses.all()]
+    
+    class Media:
+        css = {
+            'all':('css/custom.css',)
+        }
+        js = ('js/custom.js')
+
+
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(State)
 admin.site.register(City)
 admin.site.register(Neighborhood)
