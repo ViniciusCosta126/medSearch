c2c4cf218ec3935b729e95de8964245f16effdc6
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2023-10-19 09:12:34 -0300

    feat: Criacao de tela de login e registro

--- mediSearchAdmin/settings/settings.py
+++ mediSearchAdmin/settings/settings.py
@@ -107,9 +107,14 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_URL = '/logout'
+LOGOUT_REDIRECT_URL = '/login'
